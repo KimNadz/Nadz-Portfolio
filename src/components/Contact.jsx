16f1a529d70d1ea34b1bcfbe0f83d3337d405f66
JustinNadz <justinnadela2@gmail.com>
2026-01-19 14:16:22 +0800

    chnages

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -73,7 +73,7 @@ const Contact = () => {
           user_name: form.name,
           my_name: "Justin Nadela",
           user_email: form.email,
-          my_email: "Justin NadelaJustin NadelaJustin Nadela@gmail.com",
+          my_email: "Justin Nadela@gmail.com",
           user_message: form.message,
         },
         publicKey
